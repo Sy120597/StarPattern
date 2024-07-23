8f7268c7cc5e948d0241783939b49dd8b4ea59ed
Sy120597 <147397248+Sy120597@users.noreply.github.com>
2024-07-24 01:52:44 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,14 @@
+public class Reverse {
+    public static void main(String args[]){
+        int i=10;
+        int sum = 0;
+        while(i>=0){
+            System.out.println(i);
+            sum+=i;
+            i--;
+            
+            System.out.println(sum);
+        }
+    }
+}
+
